9c04159f0dbcb71c9e766bbbc1be1f59867990c8
danieljorge17 <danieljorge17@hotmail.com>
2024-10-03 21:17:12 -0300

    first commit

---
+++ src/main/java/net/daniel/neotinkers/NeoTinkersClient.java
@@ -0,0 +1,11 @@
+package net.daniel.neotinkers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NeoTinkersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This code runs on the client side
+	}
+
+}
---
+++ src/main/java/net/daniel/neotinkers/NeoTinkersDataGenerator.java
@@ -0,0 +1,11 @@
+package net.daniel.neotinkers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NeoTinkersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
